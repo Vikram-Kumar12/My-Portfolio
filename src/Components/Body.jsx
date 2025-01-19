2e57f13dbc0def93748ed56e7772ed48acec9fa7
Vikram Kumar <vikramkumar0120arav@gmail.com>
2025-01-19 13:50:43 +0530

    Update Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
-import Socialmedia from "./socialmedia";
+import Socialmedia from './Socialmedia';
 
 function Body() {
   const navigate = useNavigate();
