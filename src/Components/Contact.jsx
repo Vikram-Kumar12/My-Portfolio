98ac601d7c3acdd0d3241e851dbf2226fdf790f9
Vikram Kumar <vikramkumar0120arav@gmail.com>
2025-01-19 13:51:15 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import Socialmedia from "./socialmedia";
+import Socialmedia from './Socialmedia';
 
 function Contact() {
   const form = useRef();
